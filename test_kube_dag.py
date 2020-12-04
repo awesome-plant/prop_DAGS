0deba19ef98f022d4e5b8be6aa55518d40059c8e
drakazaka <drakazaka@hotmail.com>
2020-12-04 21:55:45 +1100

    changes again

--- test_kube_dag.py
+++ test_kube_dag.py
@@ -23,7 +23,7 @@ start = DummyOperator(task_id='run_this_first', dag=dag)
 
 passing = KubernetesPodOperator(namespace='airflow',
                           image="python:rc-slim",
-                          cmds=["Python","-c"],
+                          cmds=["python","-c"],
                           arguments=["print('hello world')"],
                           labels={"foo": "bar"},
                           name="passing-test",
@@ -34,7 +34,7 @@ passing = KubernetesPodOperator(namespace='airflow',
 
 failing = KubernetesPodOperator(namespace='airflow',
                           image="python:rc-slim",
-                          cmds=["Python","-c"],
+                          cmds=["python","-c"],
                           arguments=["print('hello world')"],
                           labels={"foo": "bar"},
                           name="fail",
