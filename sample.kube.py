4762df0f93e305dbb4ebebf7122c224c84271b63
drakazaka <drakazaka@hotmail.com>
2020-12-20 20:10:35 +1100

    asdasd

--- sample.kube.py
+++ sample.kube.py
@@ -6,7 +6,7 @@ from airflow import models
 from airflow.contrib.operators import kubernetes_pod_operator
 from airflow.contrib.operators import KubernetesOperator
 import os
-# from kubernetes.client import models as k8s
+from kubernetes.client import models as k8s
 
 args = {
     'owner': 'airflow'
@@ -30,10 +30,11 @@ YESTERDAY = datetime.datetime.now() - datetime.timedelta(days=1)
 #                                     ) 
 #                                 ]
 
-volume_mount = VolumeMount(name='xmlsave',
+volume_mount = [k8s.VolumeMount(name='xmlsave',
                             mount_path='/usr/local/airflow/xmlSave',
                             sub_path=None,
                             read_only=True)
+                ]
 
 # init_container_volume_mounts = [k8s.V1VolumeMount(name='xmlsave',
 #                                 mount_path='/usr/local/airflow/xmlSave',
@@ -64,7 +65,7 @@ try:
                     ,get_logs=True
                     ,cmds=["python","-c"]
                     ,arguments=["import time; print('hello world'); time.sleep(200); print('done')"]
-                    ,volume_mounts=[volume_mount]
+                    ,volume_mounts=volume_mount
                     # ,cmds=["./docker-run.sh"]
                     ,is_delete_operator_pod=False
                     ,dag=dag)
