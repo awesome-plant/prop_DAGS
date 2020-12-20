7832ae89f5b7c3771aaec9ce299da2ece6a2bb78
drakazaka <drakazaka@hotmail.com>
2020-12-20 22:28:29 +1100

    asdas

--- sample.kube.py
+++ sample.kube.py
@@ -66,12 +66,10 @@ try:
             start_date=YESTERDAY) as dag:
                 print("Initialized dag")
                 kubernetes_min_pod = kubernetes_pod_operator.KubernetesPodOperator(
-                    # The ID specified for the task.
                     task_id='trigger-task'
-                    # Name of task you want to run, used to generate Pod ID.
                     ,name='trigger-name'
                     ,namespace='airflow'
-                    ,in_cluster=True
+                    # ,in_cluster=True
                     ,image="python:rc-slim"
                     ,image_pull_policy='IfNotPresent'
                     ,resources={'limit_cpu' : '500m','limit_memory' : '512Mi'}
@@ -81,8 +79,8 @@ try:
                     ,arguments=["import time; print('hello world'); time.sleep(200); print('done')"]
                     # ,volumes=[volume]
                     # ,volume_mounts=[volume_mount]
-                    ,affinity=affinity
-                    ,tolerations=tolerations
+                    # ,affinity=affinity
+                    # ,tolerations=tolerations
                     # ,cmds=["./docker-run.sh"]
                     ,is_delete_operator_pod=False
                     ,dag=dag)
