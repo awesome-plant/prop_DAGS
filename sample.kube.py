50bb2f3b0f7646e2cd19ad959071bca6c6add6b0
drakazaka <drakazaka@hotmail.com>
2020-12-21 22:15:40 +1100

    asdfwrag

--- sample.kube.py
+++ sample.kube.py
@@ -5,6 +5,7 @@ import os
 from airflow import models
 from airflow.contrib.operators import kubernetes_pod_operator
 from airflow.contrib.kubernetes.volume_mount import VolumeMount
+from airflow.contrib.kubernetes.Volume import Volume
 # from airflow.contrib.operators import KubernetesOperator
 from kubernetes.client import models as k8s
 from airflow import DAG
@@ -17,13 +18,28 @@ volume_mount = VolumeMount(
                             sub_path=None,
                             read_only=False
                             )
-# volume = k8s.V1Volume(
-#     name='xmlsave'
-#     ,persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(
-#         claim_name='xmlsave'
-#         ),
-#     )
-# configmaps = [k8s.V1EnvFromSource(config_map_ref=k8s.V1ConfigMapEnvSource(name='config_name'))]
+volume_config = {
+    'persistentVolumeClaim':
+    {
+        'claimName': 'pv-xmlsave' # uses the persistentVolumeClaim given in the Kube yaml
+    }
+}
+volume = Volume(name='xmlsave', configs=volume_config) # the name here is the literal name given to volume for the pods yaml.
+affinity={
+        'nodeAffinity': {
+            'requiredDuringSchedulingIgnoredDuringExecution': {
+                'nodeSelectorTerms': [{
+                    'matchExpressions': [{
+                        'key': 'cloud.google.com/gke-nodepool',
+                        'operator': 'In',
+                        'values': [
+                            affinity,
+                        ]
+                    }]
+                }]
+            }
+        }
+    },
 
 try:
     print("Entered try block")
@@ -45,6 +61,8 @@ try:
                     ,cmds=["python","-c"]
                     ,arguments=["import time; print('hello world'); time.sleep(2); print('done')"]
                     ,volume_mounts=[volume_mount]
+                    ,volumes=[volume]
+                    ,affinty=affinity 
                     ,is_delete_operator_pod=False
                     ,dag=dag)
                 print("done")
