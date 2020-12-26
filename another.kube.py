76559f09c706c189141b78143dc8f8f9c76d75ab
drakazaka <drakazaka@hotmail.com>
2020-12-26 17:17:00 +1100

    same thing try again

--- another.kube.py
+++ another.kube.py
@@ -1,6 +1,7 @@
 # https://airflow.apache.org/docs/apache-airflow/stable/_modules/airflow/example_dags/example_kubernetes_executor_config.html
 import logging
 import os
+import time
 
 from airflow import DAG
 # from airflow.example_dags.libs.helper import print_stuff
@@ -17,18 +18,18 @@ default_args = {
 log = logging.getLogger(__name__)
 
 volume = k8s.V1Volume(
-            name='xmlsave'
-            ,persistent_volume_claim=k8s.V1HostPathVolumeSource(path='xmlsave'),
+            name='xmlsave', persistent_volume_claim=k8s.V1HostPathVolumeSource(path='xmlsave'),
         )
 volumemount = k8s.V1VolumeMount(
-                mount_path='/usr/local/airflow/xmlsave'
-                , name='persist-xmlsave'
-                , sub_path=None
-                , read_only=False
+                mount_path='/usr/local/airflow/xmlsave', name='persist-xmlsave', sub_path=None, read_only=False
             )
 
-def print_this(): 
-    print("this!")
+
+def print_this():
+    print('start wait!')
+    time.sleep(600)
+    print('end wait!')
+
 
 with DAG(
     dag_id='example_kubernetes_executor_config',
@@ -39,23 +40,30 @@ with DAG(
 ) as dag:
 
     start_task = BashOperator(
-        task_id="start_task"
-        ,bash_command='echo starting_scrape_process'
-        ,executor_config={
-            "pod_override": k8s.V1Pod(metadata=k8s.V1ObjectMeta(annotations={"test": "annotation"}))
+        task_id='start_task', bash_command='echo starting_scrape_process', executor_config={
+            'pod_override': k8s.V1Pod(metadata=k8s.V1ObjectMeta(annotations={'test': 'annotation'}))
         },
     )
+    # https://www.aylakhan.tech/?p=655
     example_task = PythonOperator(
     task_id='exmaple_task',
     python_callable=print_this,
     executor_config={
-        'KubernetesExecutor': { 'request_cpu': '1'
-                                ,'request_memory': '128Mi'
-                                ,'limit_memory': '128Mi'
-                                ,'volumes': [volume]
-                                ,'volume_mounts': [volumemount]
-                                }
-                    }
+        'KubernetesExecutor': {
+                                'request_cpu': '1'
+                                , 'request_memory': '128Mi'
+                                , 'limit_memory': '128Mi'
+                                ,'volumes': [ {
+                                    'name': 'xmlsave'
+                                    ,'persistentVolumeClaim':{'claimName': 'xmlsave' }
+                                    }
+                                ]
+                                ,'volume_mounts': [ {
+                                    'name': 'xmlsave',
+                                    'mountPath': '/usr/local/airflow/xmlsave'
+                                    }
+                                ]
+                            }
     )
 
     start_task >> example_task
