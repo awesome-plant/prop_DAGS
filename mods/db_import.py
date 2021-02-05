95cacf281cd5009a523b4f50f04f792c89017b4a
drakazaka <drakazaka@hotmail.com>
2021-02-06 00:22:07 +1100

    srthrtthj

--- mods/db_import.py
+++ mods/db_import.py
@@ -67,8 +67,7 @@ def saveProxies(ps_user, ps_pass, ps_host, ps_port, ps_db, update, df_proxy_list
                 conn.commit()
                 # https://stackoverflow.com/questions/18390574/how-to-delete-duplicate-rows-in-sql-server
                 # delete duplicates 
-                cur.execute("""
-                            delete from sc_land.sc_proxy_raw a 
+                cur.execute("""delete from sc_land.sc_proxy_raw a 
                             	using sc_land.sc_proxy_raw b 
                             	where a. table_id > b.table_id 
                             	and a.proxy = b.proxy   
