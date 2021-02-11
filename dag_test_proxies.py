a75169335905c90db40bf6eb09fcb090ba7bb076
drakazaka <drakazaka@hotmail.com>
2021-02-11 21:18:57 +1100

    awefsegfaewgerg

--- dag_test_proxies.py
+++ dag_test_proxies.py
@@ -52,16 +52,45 @@ with DAG(
 ) as dag:
     group=1 
     count=0 
-    split_old = DummyOperator(task_id='dummy_starter',trigger_rule='all_done')
-    split_new = DummyOperator(task_id='dummy_splitter_' + str(group),trigger_rule='all_done' )
+    # split_old = DummyOperator(task_id='dummy_starter',trigger_rule='all_done')
+    split_old = KubernetesPodOperator(
+                namespace='airflow'
+                , name="refresh_ip_" + 0
+                , task_id="refresh_ip_" + 0
+                , image="babadillo12345/airflow-plant:scrape_worker-1.1"
+                , cmds=["bash", "-cx"]
+                , arguments=["git clone https://github.com/awesome-plant/prop_DAGS.git && python prop_DAGS/mods/proxy.py -mod refresh_ip"]  
+                , image_pull_policy="IfNotPresent"
+                , resources={'limit_cpu' : '50m','limit_memory' : '512Mi'}  
+                , labels={"foo": "bar"}
+                , volumes=[volume]
+                , volume_mounts=[volume_mount]
+                , is_delete_operator_pod=True
+                , in_cluster=True
+            )
+    split_new = KubernetesPodOperator(
+                namespace='airflow'
+                , name="refresh_ip_" + str(group)
+                , task_id="refresh_ip_" + str(group)
+                , image="babadillo12345/airflow-plant:scrape_worker-1.1"
+                , cmds=["bash", "-cx"]
+                , arguments=["git clone https://github.com/awesome-plant/prop_DAGS.git && python prop_DAGS/mods/proxy.py -mod refresh_ip"]  
+                , image_pull_policy="IfNotPresent"
+                , resources={'limit_cpu' : '50m','limit_memory' : '512Mi'}  
+                , labels={"foo": "bar"}
+                , volumes=[volume]
+                , volume_mounts=[volume_mount]
+                , is_delete_operator_pod=True
+                , in_cluster=True
+            )
     
     for sql_start in range(0, math.ceil(proxy_count/batch_size)):
         if count == batch_g_size:
             split_old = split_new
             count=0 
             group+=1
-            split_new = DummyOperator(task_id='dummy_splitter_' + str(group),trigger_rule='all_done')
-            refresh_ip = KubernetesPodOperator(
+            # split_new = DummyOperator(task_id='dummy_splitter_' + str(group),trigger_rule='all_done')
+            split_new = KubernetesPodOperator(
                 namespace='airflow'
                 , name="refresh_ip_" + str(group)
                 , task_id="refresh_ip_" + str(group)
@@ -91,5 +120,5 @@ with DAG(
                 , is_delete_operator_pod=True
                 , in_cluster=True
             )
-        split_old >> proxy_test >> refresh_ip >> split_new
+        split_old >> proxy_test >> split_new
         count+=1
