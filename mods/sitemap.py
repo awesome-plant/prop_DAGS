b52d6a985151a4d46e1c9cf7e9537636628c2551
drakazaka <drakazaka@hotmail.com>
2021-03-01 21:35:27 +1100

    asf

--- mods/sitemap.py
+++ mods/sitemap.py
@@ -191,13 +191,6 @@ def site_ScrapeChildUrl(sql_start, sql_size):
                 headers={ 'User-Agent': proxy.getHeader(random.randint(0,249))  } 
                 # s_filename, s_fileid
                 r = requests.get(site_url + row['s_filename'], proxies=proxies,headers=headers, timeout=timeout,verify=False, allow_redirects=True)
-                # if len(r.text) > 50:
-                #     scrape_status=True
-                #     root = etree.fromstring(r.content)
-                #     body=root.xpath('//ns:Contents',namespaces={'ns':"http://s3.amazonaws.com/doc/2006-03-01/"})
-                # elif len(r.text) < 50: 
-                #     print("bot blocked IP:",proxy, "lc:", str(loopcount))
-                #     headers={ 'User-Agent': proxy.getHeader(random.randint(0,249))  } 
             except Exception as e:
                 print("prox:", str(proxies), "lc:",str(loopcount), "-error:", str(e) )
                 scrape_status=False
