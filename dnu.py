e02bec0410c3783d2d58adf8ffa700d4efa9edc4
drakazaka <drakazaka@hotmail.com>
2021-03-08 15:36:40 +1100

    grg

--- dnu.py
+++ dnu.py
@@ -11,31 +11,42 @@ from selenium import webdriver
 import time 
 import json 
 import pandas as pd
+
 start_time = time.time()
-# timeout=30    # cat='Sold'    # prop_id='131116802'    # site_url='https://www.realestate.com.au/sold/property-house-nsw-cootamundra-131116802'
-prox, proxy_type = scrape_site.workProxy()
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument('--user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36')
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--proxy-server="+ proxy_type +"://" + prox)
-chrome_options.add_argument('--blink-settings=imagesEnabled=false')
-chrome_options.add_argument("--disable-popup-blocking")
-chrome_options.add_argument("--window-size=1420,1080")
-chrome_options.add_argument('--disable-blink-features=AutomationControlled')
-prefs = {}
-prefs = {"profile.managed_default_content_settings.images": 2}
-chrome_options.add_experimental_option("prefs", prefs)
-browser = webdriver.Chrome(options=chrome_options)
+ from selenium import webdriver
+    import time 
+    import json 
+    import pandas as pd
+    start_time = time.time()
+    scrape_status=False
+    while scrape_status==False:
+        try:
+            prox, proxy_type = scrape_site.workProxy()
+            chrome_options = webdriver.ChromeOptions()
+            chrome_options.add_argument("--headless")
+            chrome_options.add_argument('--user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36')
+            chrome_options.add_argument("--no-sandbox")
+            chrome_options.add_argument("--disable-dev-shm-usage")
+            chrome_options.add_argument("--proxy-server="+ proxy_type +"://" + prox)
+            chrome_options.add_argument('--blink-settings=imagesEnabled=false')
+            chrome_options.add_argument("--disable-popup-blocking")
+            chrome_options.add_argument("--window-size=1420,1080")
+            chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+            prefs = {}
+            prefs = {"profile.managed_default_content_settings.images": 2}
+            chrome_options.add_experimental_option("prefs", prefs)
+            browser = webdriver.Chrome(options=chrome_options)
 
-browser.get(site_url)
-time.sleep(sleep_time)
-webpage = browser.page_source
-#get cookies used for traditional webscraping
-cook_reauid= browser.get_cookie('reauid')['value']
-cook_bm_aksd= browser.get_cookie('bm_aksd')['value']
-browser.quit()
+            browser.get(site_url)
+            time.sleep(sleep_time)
+            webpage = browser.page_source
+            #get cookies used for traditional webscraping
+            cook_reauid= browser.get_cookie('reauid')['value']
+            cook_bm_aksd= browser.get_cookie('bm_aksd')['value']
+            browser.quit()
+            scrape_status=True
+        except Exception as e: 
+            print('error: ', str(e))
 
-print("selenium headers s:", time.time() - start_time)
+    print("selenium headers s:", time.time() - start_time)
