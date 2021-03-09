d66669bb9cb5db066e002424e66e0d74e0b40833
drakazaka <drakazaka@hotmail.com>
2021-03-09 22:32:00 +1100

    ergrg

--- dag_get_sitemap_child.py
+++ dag_get_sitemap_child.py
@@ -45,7 +45,7 @@ default_args = {
 
 #get current proxies in db
 batch_size=5
-batch_g_size=10 #used to remove that pod timeout error 
+batch_g_size=5 #used to remove that pod timeout error 
 CP_count=db_import.getChildPagesCount(ps_user="postgres", ps_pass="root", ps_host="172.22.114.65", ps_port="5432", ps_db="scrape_db")
 
 #iterate to run 
