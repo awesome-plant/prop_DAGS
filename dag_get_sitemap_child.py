2e90d9b140580161f86d14735746e3610851d56b
drakazaka <drakazaka@hotmail.com>
2021-03-09 20:55:29 +1100

    sdgdfbg

--- dag_get_sitemap_child.py
+++ dag_get_sitemap_child.py
@@ -44,8 +44,8 @@ default_args = {
 }
 
 #get current proxies in db
-batch_size=20
-batch_g_size=5 #used to remove that pod timeout error 
+batch_size=5
+batch_g_size=10 #used to remove that pod timeout error 
 CP_count=db_import.getChildPagesCount(ps_user="postgres", ps_pass="root", ps_host="172.22.114.65", ps_port="5432", ps_db="scrape_db")
 
 #iterate to run 
@@ -76,7 +76,7 @@ with DAG(
                 , task_id="get_smp-" + mod + "_" + str(sql_start)
                 , image="babadillo12345/airflow-plant:scrape_worker-1.2"
                 , cmds=["bash", "-cx"]
-                , arguments=["git clone https://github.com/awesome-plant/prop_DAGS.git && python prop_DAGS/mods/realestate.py -mod " + mod + " -st " + str(sql_start*batch_size) + " -si " + str(batch_size)]  
+                , arguments=["git clone https://github.com/awesome-plant/prop_DAGS.git && python prop_DAGS/mods/realestate.py -mod " + mod + " -st " + str(sql_start*20) + " -si " + str(batch_size)]  
                 , image_pull_policy="IfNotPresent"
                 , resources={'limit_cpu' : '50m','limit_memory' : '512Mi'}  
                 , labels={"foo": "bar"}
