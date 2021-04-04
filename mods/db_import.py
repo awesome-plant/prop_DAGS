fcb7d59d333b6ac3f11960e9946326a3c7ebc645
drakazaka <drakazaka@hotmail.com>
2021-04-04 15:43:33 +1000

    ahth

--- mods/db_import.py
+++ mods/db_import.py
@@ -311,6 +311,7 @@ def cleanupSiteMapChild(ps_user, ps_pass, ps_host, ps_port, ps_db):
                 new_count=result[0]
                 print("new count:", new_count)
                 print('child pages removed:', str(int(old_count) - int(new_count)) )
+            update_status=True
         except Exception as e: 
             update_status=False
             rc+=1
